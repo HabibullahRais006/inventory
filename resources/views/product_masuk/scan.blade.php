4244c775bba5e4261ae8452bfbf659ca52b193f5
HabibullahRais006 <hbrais06@gmail.com>
2022-06-17 03:40:33 +0700

    abcd

---
+++ resources/views/product_masuk/scan.blade.php
@@ -0,0 +1,58 @@
+<div class="modal fade" id="modal-form-qr" tabindex="1" role="dialog" aria-hidden="true" data-backdrop="static">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <form  id="form-item" method="post" class="form-horizontal" data-toggle="validator" enctype="multipart/form-data" >
+                {{-- {{ csrf_field() }} {{ method_field('POST') }} --}}
+
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span></button>
+                    <h3 class="modal-title"></h3>
+                </div>
+
+
+                <div class="modal-body">
+                    <input type="hidden" id="id" name="id">
+
+
+                    <div class="box-body">
+                        
+                        <div id="reader" width="60px"></div>
+                    </div>
+                    <!-- /.box-body -->
+
+                </div>
+
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Cancel</button>
+                </div>
+            </form>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
+<div id="reader" width="600px"></div>
+
+<!-- /.modal -->
+@push('script')
+<script src="https://unpkg.com/html5-qrcode" type="text/javascript"></script>
+<script>
+    function onScanSuccess(decodedText, decodedResult) {
+  // handle the scanned code as you like, for example:
+  console.log(`Code matched = ${decodedText}`, decodedResult);
+}
+
+function onScanFailure(error) {
+  // handle scan failure, usually better to ignore and keep scanning.
+  // for example:
+  console.warn(`Code scan error = ${error}`);
+}
+
+let html5QrcodeScanner = new Html5QrcodeScanner(
+  "reader",
+  { fps: 10, qrbox: {width: 250, height: 250} },
+  /* verbose= */ false);
+html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+</script>
+@endpush
